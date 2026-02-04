3bed423b27200e498a27fe8d0f0d11f5de0ba136
Marksan Mallanao <marksanmallanao28@gmail.com>
2026-02-04 22:57:59 +0800

    Final V form

--- V form.py
+++ V form.py
@@ -258,5 +258,6 @@ def admin():
  except:
   return "No answers yet."
 
-if __name__=="__main__":
- app.run(host="0.0.0.0", port=10000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
+
